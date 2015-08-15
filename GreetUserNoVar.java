e60e90c051dc5e0d0bc75ac185af314b976cbd6f
dnowak <dawid.nowak886@gmail.com>
2015-08-15 19:17:27 +0200

    added GreetUserNoVar.java and GreetUserNoVar.class

---
+++ GreetUserNoVar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; 
+
+public class GreetUserNoVar {  
+
+	public static void main(String args[]) {  
+
+		Scanner read = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		System.out.println("Hello " + read.nextLine()); 
+
+	} 
+} 
+
+
+
+
+
+
+
